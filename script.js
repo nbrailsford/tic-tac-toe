0759303349737301d2e496f9e6f66412e3b23a4c
nbrailsford <nbrailsford@msn.com>
2020-05-12 16:22:14 -0600

    added basic ai

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function startGame() {
 function turnClick(square) {
   if (typeof origBoard[square.target.id] == "number") {
     turn(square.target.id, huPlayer);
-    if (checkTie()) turn(bestSpot(), aiPlayer);
+    if (!checkTie()) turn(bestSpot(), aiPlayer);
   }
 }
 
@@ -59,4 +59,30 @@ function gameOver(gameWon) {
   for (var i = 0; i < cells.length; i++) {
     cells[i].removeEventListener("click", turnClick, false);
   }
+  declareWinner(gameWon.player == huPlayer ? "You win!" : "You lose.");
+}
+
+function declareWinner(who) {
+  document.querySelector(".endgame").style.display = "block";
+  document.querySelector(".endgame .text").innerText = who;
+}
+
+function emptySquares() {
+  return origBoard.filter((s) => typeof s == "number");
+}
+
+function bestSpot() {
+  return emptySquares()[0];
+}
+
+function checkTie() {
+  if (emptySquares().length == 0) {
+    for (var i = 0; i < cells.length; i++) {
+      cells[i].style.backgroundColor = "green";
+      cells[i].removeEventListener("click", turnClick, false);
+    }
+    declareWinner("Tie Game!");
+    return true;
+  }
+  return false;
 }
